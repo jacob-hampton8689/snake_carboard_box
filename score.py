8a23ccb945b99e51d32361a25a879bb56be019ca
ZachariaJanes <167346342+ZachariaJanes@users.noreply.github.com>
2024-04-22 21:01:56 +0000

    sdfsdf

--- score.py
+++ score.py
@@ -2,9 +2,9 @@ import turtle as tur
 
 
 def screen (x , y , name):
-    tur.title(name)
     sc = tur.Screen()
     sc.setup(x,y,startx=0,starty=0)
+    sc.title(name)
 
 if __name__ == '__main__':
     screen(600 , 620 , 'snake')
