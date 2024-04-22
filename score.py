373bf9075fd4418c3c26b9bdc404467565aea777
ZachariaJanes <167346342+ZachariaJanes@users.noreply.github.com>
2024-04-22 17:46:30 +0000

    s

--- score.py
+++ score.py
@@ -1,10 +1,14 @@
-import turtle as tur
+import turtle
+tur = turtle.Turtle()
+import os
+
 
 def screen (x , y , name):
-    tur.title(str(name))
-    sc = tur.Screen()
-    sc.setup(x,y,startx=0,starty=0)
-    return sc
+    tur.title(name)
+    tur.geometry(f'{x}x{y}')
 
 if __name__ == '__main__':
-    screen1 = screen(600 , 620 , 'snake')
+    os.environ.__setitem__('Display' , ':0.0')
+    screen(600 , 620 , 'snake')
+    while True:
+        tur.exitonclick
