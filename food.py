90f5abd73d7a5ea7ddd72244829d8459492d9ce9
Jacob <jacob.hampton.8689@mail.linnbenton.edu>
2024-04-30 10:20:44 -0700

    started two classes

--- food.py
+++ food.py
@@ -1,5 +1,23 @@
 import turtle as tur
 import random as ran
 
+foodY = ran.randint(-280, 280)
+foodX = ran.randint(-280,280)
+
+
 class Food:
-    def __init__(self, )
+    def __init__(self, shape, color):
+        shape = tur.shape("square")
+        color = tur.color("red")
+        self.shape = shape
+        self.color = color
+        self.posy = foodX
+        self.posy = foodY
+        self.location = (foodX, foodY)
+       
+
+    def teleporting(self):
+        self.location = tur.goto(foodX, foodY)
+
+
+        
--- score.py
+++ score.py
@@ -1,9 +1,17 @@
-from turtle import *
+import turtle as tur
 
 
-forward(10)
-left(90)
-forward(10)
+class Score:
+    def __init__(self, width, height, score, title):
+        self.width = width
+        self.height = height
+        self.score = score
+        self.title = title
 
 
+    def update_score(self):
+        self.score += 1
+
+    def screen_title(title):
+        title = tur.title("Snake!")
 
