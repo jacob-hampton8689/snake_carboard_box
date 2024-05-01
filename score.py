874d79f263b183e783587937ae50b285b4aca8ab
Jacob <jacob.hampton.8689@mail.linnbenton.edu>
2024-05-01 10:56:35 -0700

    nasty code

--- score.py
+++ score.py
@@ -1,17 +1,32 @@
 import turtle as tur
-
-
+snake_score = 0
+high_score = 0
 class Score:
-  def __init__(self, width, height, color,  score, title):
+  def __init__(self, width, height, color,title):
     self.width = width
     self.height = height
     self.color = color
-    self.score = score
+    self.score = snake_score
+    self.all_time_score = high_score
     self.title = title
 
+  def display_score():
+     pen = tur.Turtle()
+     pen.speed(0)
+     pen.shape('circle')
+     pen.color('white')
+     pen.penup()
+     pen.hideturtle()
+     pen.goto(0, 310)
+     tur.write("Score: " + str(snake_score) + " High score: " + str(high_score), font= 
+    ("Arial", 24, "normal"))
+     
 
   def update_score(self):
-     self.score += 1
+     tur.clear()
+     tur.write("Score: {} High Score: {} ".format(self.score, self.all_time_score), allign="center", font=("arial", 24, "bold"))
+     self.score += 10
+     self.all_time_score += high_score
 
   def screen_title(self, title):
      title = tur.title(title)
@@ -23,9 +38,12 @@ class Score:
      tur.exitonclick()
     
 def test():
-   screen = Score(600, 600, "black", 0, "Snake!")
+   screen = Score(600, 680, "black", "Snake!")
    screen.screen_title(screen.title)
+   screen.display_score()
    screen.screen_elements(screen.width, screen.height, screen.color)
+
+
 
 if __name__ == '__main__':
    test()
