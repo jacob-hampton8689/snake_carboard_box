22abf603de1a3a005fd60b7d8ba59a2423fa7488
zach <zacharia.janes.6893@mail.linnenton.edu>
2024-05-02 09:59:53 -0700

    popo

--- snake.py
+++ snake.py
@@ -1,26 +1,26 @@
 from turtle import Turtle
 from turtle import Screen as tscreen
 from score import grid
-tur = None
+screen = tscreen()
 
-
-def makesquare (color , width , turtle):
-    turtle.down()
-    turtle.color(color)
-    turtle.begin_fill()
-    turtle.setheading(270)
-    turtle.forward(width)
-    turtle.left(90)
-    turtle.forward(width)
-    turtle.left(90)
-    turtle.forward(width)
-    turtle.left(90)
-    turtle.forward(width)
-    turtle.end_fill()
-    turtle.up()
+def makesquare (color , width , dood):
+    dood.down()
+    dood.color(color)
+    dood.begin_fill()
+    dood.setheading(270)
+    dood.forward(width)
+    dood.left(90)
+    dood.forward(width)
+    dood.left(90)
+    dood.forward(width)
+    dood.left(90)
+    dood.forward(width)
+    dood.end_fill()
+    dood.up()
 
 class snake:
     def __init__(self , x , y , color , width , bgcol , turlte):
+        global screen
         self.x = x
         self.y = y
         self.head = [x , y]
@@ -30,7 +30,8 @@ class snake:
         self.width = width
         self.bg = bgcol
         self.screen = screen
-        self.turtle = turlte
+        self.dood = turlte
+        self.hit_food = False
    
     def move(self):
         xpos = self.head[0]
@@ -45,27 +46,38 @@ class snake:
             ypos -= 1
         self.head = [xpos , ypos]
         self.body.append([xpos , ypos])
-        self.draw_()
+        self.draw()
 
-    def draw_(self):
-        self.turtle.tracer(0)
-        print(self.turtle)
-        print(self.turtle.position())   
-        self.turtle.up()
-        self.turtle.goto(grid[self.head[0]] , grid[self.head[1]])
-        makesquare(self.color , self.width , self.turtle)
-        self.turtle.goto(grid[self.body[0][0]] , grid[self.body[0][1]])
-        makesquare(self.bg , self.width , self.turtle)
-        del self.body[0]
+    def draw(self):
+        screen.tracer(0)  
+        print(self.dood)
+        print(self.dood.position())   
+        self.dood.up()
+        self.dood.goto(grid[self.head[0]] , grid[self.head[1]])
+        makesquare(self.color , self.width , self.dood)
+        self.dood.goto(grid[self.body[0][0]] , grid[self.body[0][1]])
+        makesquare(self.bg , self.width , self.dood)
+        if self.hit_food == False:
+            del self.body[0]
+        screen.update()
+
+    def collision(self):
+        for i in self.body:
+            if i == self.head:
+                print('gameover')
+                self.game_over()
+
+
+    def game_over():
+        pass
 
 
 if __name__ == '__main__':
-    turtle = Turtle()
-    turtle.speed('fastest')
-    turtle.hideturtle()
-    screen = tscreen()
+    dood = Turtle()
+    dood.speed('fastest')
+    dood.hideturtle()
     screen.screensize(600 , 600)
-    body = snake(grid[0] , grid[0] , 'green' , 30 , 'white' , turtle)
+    body = snake(grid[0] , grid[0] , 'green' , 30 , 'white' , dood)
     body.move()
     print(body.body)
     body.move()
